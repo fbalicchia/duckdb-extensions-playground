966618a43075800bd10701ea3735de0a884eae4b
fbalicchia <fbalicchia@cuebiq.com>
2022-05-08 08:32:56 +0200

    add h3 example

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # DuckDB <> Extension Playground
 
+This is a simple project that permits me to play and understand Duckdb UDF function [API](https://duckdb.org/docs/api/cpp).
+The main class is `first_extension` which provides two functions. One is the classic hello world the other one is a simple 
+a function that uses [H3](https://github.com/uber/h3) to calculate the distance in KM between two points expressed in geohash
 
 
+Load submodule
+```
+git submodule update --init --recursive 
+```
 
 
 To build, type 
 ```
-make update; make
+make
 ```
 
 To run, run the bundled `duckdb` shell:
@@ -21,4 +28,9 @@ LOAD 'build/release/first_extensions.duckdb_extension';
 
 ```SQL
  select hello('World from Italy');
+```
+
+
+```SQL
+select distance_in_km('8f2830828052d25', '8f283082a30e623');
 ```
--- first_extensions.cpp
+++ first_extensions.cpp
@@ -1,11 +1,50 @@
 #define DUCKDB_BUILD_LOADABLE_EXTENSION
 #include "duckdb.hpp"
+#include "h3api.h"
 
 using namespace duckdb;
+
+
+#define R 6371.0088
+
+
+double haversineDistance(double th1, double ph1, double th2, double ph2) {
+    double dx, dy, dz;
+    ph1 -= ph2;
+
+    dz = sin(th1) - sin(th2);
+    dx = cos(ph1) * cos(th1) - cos(th2);
+    dy = sin(ph1) * cos(th1);
+    return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * R;
+}
+
+
+//this function is a H3 example porting get in input two geohash and 
+// return distance expressed in variuos format  
+inline string_t distance_fun(string_t origin, string_t destination) {
+    
+    H3Index h3HQ1;
+    H3Index h3HQ2;
+    stringToH3(origin.GetString().c_str(), &h3HQ1);
+    stringToH3(destination.GetString().c_str(), &h3HQ2);
+    LatLng geoHQ1, geoHQ2;
+    cellToLatLng(h3HQ1, &geoHQ1);
+    cellToLatLng(h3HQ2, &geoHQ2);
+
+    int64_t distance;
+    assert(gridDistance(h3HQ1, h3HQ2, &distance) == E_SUCCESS);
+
+    char result[200];
+    snprintf(result,200,"%f",haversineDistance(geoHQ1.lat, geoHQ1.lng, geoHQ2.lat, geoHQ2.lng));
+    return result;
+
+}
+
 
 inline string_t hello_fun(string_t what) {
 	return "Hello, " + what.GetString();
 }
+
 
 extern "C" {
 DUCKDB_EXTENSION_API void first_extensions_init(duckdb::DatabaseInstance &db) {
@@ -13,6 +52,11 @@ DUCKDB_EXTENSION_API void first_extensions_init(duckdb::DatabaseInstance &db) {
 	con.BeginTransaction();
 	con.CreateScalarFunction<string_t, string_t>("hello", {LogicalType(LogicalTypeId::VARCHAR)},
 	                                             LogicalType(LogicalTypeId::VARCHAR), &hello_fun);
+
+    con.CreateScalarFunction<string_t, string_t,string_t>("distance_in_km", {LogicalType(LogicalTypeId::VARCHAR), LogicalType(LogicalTypeId::VARCHAR)},
+	                                             LogicalType(LogicalTypeId::VARCHAR), &distance_fun);
+
+
 	con.Commit();
 }
 
